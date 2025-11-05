928764bb66a9882ff2196c2e368cb725eeb7356e
ParameciumMax <a41923960@gmail.com>
2025-11-05 16:11:13 +0800

    251105

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var server = express();
+var bodyParser = require("body-parser");
+
+
+server.use(express.static(__dirname + "/myweb/"));
+server.use(bodyParser.urlencoded());
+
+server.get("/", (req,res)=>{
+    res.send("Hello world!");
+})
+
+server.get("/about", (req,res)=>{
+    res.send("My first NodeJS server!");
+})
+
+server.listen(8080)
